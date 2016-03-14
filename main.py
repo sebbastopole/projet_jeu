ef208318668a36e41ef5b7433b674780cc02e319
Cominu Billal <cominu.b@gmail.com>
2016-03-14 23:57:00 +0100

    mouvement

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from player import *
 
 def main():
     ui = UserInterface()
-    player = Player(0,50)
+    player = Player(0,500)
     player.move(50,50)
     ui.addPlayer(player)
     ui.drawPlayers()
+    i=0
+    while(i<10000):
+        time.sleep(1)
+        player.move(10,10)
+        i+=1
     
 main()
 
